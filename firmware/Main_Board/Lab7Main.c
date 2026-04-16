89f471b6c20d3d74a58acae159a0909c7ad115ce
Toby Nguyen <tobyanguyen1@gmail.com>
2026-04-15 20:16:25 -0500

    led chaser launchpad select lines

--- firmware/Main_Board/Lab7Main.c
+++ firmware/Main_Board/Lab7Main.c
@@ -13,23 +13,46 @@
 
 #include "../lib/myUART.h"
 
+#define sel0 20 // PB20
+#define sel1 24 // PB24
+#define sel2 3 // PB3
+#define sel3 4 // PB4
 
 void PLL_Init(void) { // set phase lock loop (PLL)
-  // Clock_Init40MHz(); // run this line for 40MHz
   Clock_Init80MHz(0);   // run this line for 80MHz
 }
 
 void TIMG7_IRQHandler() {
   GPIOA->DOUTTGL31_0 ^= (1 << 16);
+  static uint8_t count = 0;
+  if(count > 16) {
+    count = 0;
+  }
+  uint8_t count0 = count & 1;
+  uint8_t count1 = (count & (1 << 1)) >> 1;
+  uint8_t count2 = (count & (1 << 2)) >> 2;
+  uint8_t count3 = (count & (1 << 3)) >> 3;
+  GPIOB->DOUT31_0 = (count0 << sel0) | (count1 << sel1) | (count2 << sel2) | (count3 << sel3);
+  count++;
 }
 
-
 void initLED(void){
-  // PA16, LP
+  // PA16, LP RUN LED
   IOMUX->SECCFG.PINCM[PA16INDEX] = 0x00000081;
   GPIOA->DOE31_0 |= 0x10000;
-  initUART();
 
+  // PB24
+  // PB20
+  // PB4
+  // PB3
+  GPIOB->DOE31_0 |= (1 << 24) | (1 << 20) | (1 << 4) | (1 << 3);
+  IOMUX->SECCFG.PINCM[PB24INDEX] = 0x00000081;
+  IOMUX->SECCFG.PINCM[PB20INDEX] = 0x00000081;
+  IOMUX->SECCFG.PINCM[PB4INDEX] = 0x00000081;
+  IOMUX->SECCFG.PINCM[PB3INDEX] = 0x00000081;
+
+
+  // GPIOA->DOUTTGL31_0 = (1 << 16);
   TimerG7_IntArm(UINT16_MAX, 0, 0);
 }
 
@@ -39,6 +62,7 @@ int main(void){ // main1
   PLL_Init(); // set bus speed
   LaunchPad_Init();
   initLED();
+  initUART();
   __enable_irq();
   while(1){
     
